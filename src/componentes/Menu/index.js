def4ebc1282624ab2fc5bfb4c45015a96d352aa1
André <andrehcf@gmail.com>
2020-07-29 14:06:52 -0300

    Arquivos Iniciais

---
+++ src/componentes/Menu/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Menu.css';
+import Logo from '../../assets/img/Logo.png';
+
+import Button from '../Button';
+
+
+
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <Link to="/">
+                <img className="Logo" src={Logo} alt="Logo"/>
+            </Link>
+
+        <Button as={Link} className="ButtonLink" to="/cadastro/video">
+            Novo Vídeo
+        </Button>
+
+  
+
+        </nav>
+    )
+}
+
+export default Menu;
